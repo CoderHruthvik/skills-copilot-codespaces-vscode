a126c9017d69060a34ae53dabdb30b30e8a5e14b
HruthVik <124189374+CoderHruthvik@users.noreply.github.com>
2023-08-29 20:00:08 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,4 +1,15 @@
-// create web server
-// 1. load modules
+// create web server with express
+const express = require('express');
+const router = express.Router();
+
+// import models
+const Comment = require('../models/Comment');
+const Post = require('../models/Post');
+
+// create route to post comments
+router.post('/', async (req, res) => {
+    // create comment
+    const comment = new Comment({
+        content: req.body.content,
 
 
