bcf555dc6996458fac99f8ee99a76548a74b9a18
HruthVik <124189374+CoderHruthvik@users.noreply.github.com>
2023-08-29 20:02:12 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,15 +1,65 @@
 // create web server with express
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const app = express()
+// import body-parser
+const bodyParser = require('body-parser')
+// import mongoose
+const mongoose = require('mongoose')
+// import Comment model
+const Comment = require('./models/comment')
+// import Post model
+const Post = require('./models/post')
+// import cors
+const cors = require('cors')
+// import dotenv
+const dotenv = require('dotenv')
+dotenv.config()
 
-// import models
-const Comment = require('../models/Comment');
-const Post = require('../models/Post');
+// connect to mongodb
+mongoose
+  .connect(process.env.MONGODB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log('Connected to MongoDB')
+  })
+  .catch(err => {
+    console.log(err)
+  })
 
-// create route to post comments
-router.post('/', async (req, res) => {
-    // create comment
-    const comment = new Comment({
-        content: req.body.content,
+// use cors
+app.use(cors())
+// use body-parser
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// get all comments
+app.get('/comments', (req, res) => {
+  Comment.find()
+    .then(comments => {
+      res.json(comments)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+})
+
+// get all comments for a post
+app.get('/comments/:id', (req, res) => {
+  Comment.find({ post: req.params.id })
+    .then(comments => {
+      res.json(comments)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+})
+
+// post a comment
+app.post('/comments', (req, res) => {
+  const newComment = new Comment({
+    post: req.body.post,
+    body: req.body.body,
 
 
