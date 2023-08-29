91daaea85817ec4e490bf4a98fb2df583f009e5c
HruthVik <124189374+CoderHruthvik@users.noreply.github.com>
2023-08-29 20:07:59 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,15 +1,90 @@
-// create web server
-// 1. use express
+// Create web server
+// 1. Create a web server
+// 2. Create a route for GET /comments
+// 3. Create a route for GET /comments/:id
+// 4. Create a route for POST /comments
+// 5. Create a route for PUT /comments/:id
+// 6. Create a route for DELETE /comments/:id
+// 7. Start the server
+
 const express = require('express');
-// 2. create web server object
-const router = express.Router();
-// 3. import controller
-const commentsCtrl = require('../controllers/comments');
-// 4. set route
-router.post('/comments', commentsCtrl.createComment);
-router.get('/comments', commentsCtrl.getComments);
-router.get('/comments/:id', commentsCtrl.getComment);
-router.put('/comments/:id', commentsCtrl.updateComment);
-router.delete('/comments/:id', commentsCtrl.removeComment);
-// 5. export router
-module.exports = router;
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const comments = [
+    {
+        id: 1,
+        username: 'alice',
+        comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    },
+    {
+        id: 2,
+        username: 'bob',
+        comment: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+    },
+    {
+        id: 3,
+        username: 'charlie',
+        comment: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
+    },
+    {
+        id: 4,
+        username: 'dave',
+        comment: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    }
+];
+
+app.get('/comments', (req, res) => {
+    res.json(comments);
+});
+
+app.get('/comments/:id', (req, res) => {
+    const id = req.params.id;
+    const comment = comments.find(comment => comment.id === parseInt(id));
+    if (!comment) {
+        res.status(404).send('Comment not found');
+        return;
+    }
+    res.json(comment);
+});
+
+app.post('/comments', (req, res) => {
+    const comment = req.body;
+    comment.id = comments.length + 1;
+    comments.push(comment);
+    res.status(201).json(comment);
+});
+
+app.put('/comments/:id', (req, res) => {
+    const id = req.params.id;
+    const comment = comments.find(comment => comment.id === parseInt(id));
+    if (!comment) {
+        res.status(404).send('Comment not found');
+        return;
+    }
+    comment.username = req.body.username;
+    comment.comment = req.body.comment;
+    res.json(comment);
+});
+
+app.delete('/comments/:id', (req, res) => {
+    const id = req.params.id;
+    const comment = comments.find(comment => comment.id === parseInt(id));
+    if (!comment) {
+        res.status(404).send('Comment not found');
+        return;
+    }
+    const index = comments.indexOf(comment);
+    comments.splice(index, 1);
+    res.json(comment);
+});
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
